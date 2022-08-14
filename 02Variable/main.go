d8d041ab281e713e3650506067b3b7412aca7c41
thejyotipatel <jyotipatelfyit77100@gmail.com>
2022-08-14 20:12:11 +0530

    init

---
+++ 02Variable/main.go
@@ -0,0 +1,43 @@
+package main
+import "fmt"
+
+// Token := "lkvjdjb" // error
+
+const IP float32 = 1.34 // public
+
+func main()  {
+	// var name string = "go language"
+	// fmt.Println(name)
+	// fmt.Printf("variable is of Type: %T \n", name)
+	
+	// var isActive bool = true
+	// fmt.Println(isActive)
+	// fmt.Printf("variable is of Type: %T \n", isActive)
+	
+	// var numberunit uint32 = 255
+	// fmt.Println(numberunit)
+	// fmt.Printf("variable is of Type: %T \n", numberunit)
+	
+	// var float32value float32 = 255.3768548897573872
+	// fmt.Println(float32value)
+	// fmt.Printf("variable is of Type: %T \n", float32value)
+
+	// default values and some aliases
+	// var avar int
+	// fmt.Println(avar)
+	// fmt.Printf("variable is of Type: %T \n", avar)
+
+	// implicit type
+	// var bvar = "go language"
+	// fmt.Println(bvar)
+
+	// no var style
+	// cvar := 345
+	// fmt.Println(cvar)
+
+	// constants
+	fmt.Println(IP)
+	fmt.Printf("variable is of Type: %T \n", IP)
+
+
+}
