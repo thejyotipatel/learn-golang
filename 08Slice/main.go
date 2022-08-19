eff1632401d40b7f2561ffacd90c446591b68e30
thejyotipatel <jyotipatelfyit77100@gmail.com>
2022-08-19 22:04:46 +0530

    slice

--- 08Slice/main.go
+++ 08Slice/main.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("Slice")
 
 	var sliceList = []string{"apple", "banana","mango"}
 
+	fmt.Println("Slice :", sliceList)
 	fmt.Printf("Slice type: %T\n", sliceList)
+
+	sliceList = append(sliceList, "orange")
+	fmt.Println("Slice updated: ", sliceList)
+	
+	// slice the slice
+	fmt.Println("Slice 1: - ", sliceList[1:])
+	fmt.Println("Slice 1:3 - ", sliceList[1:3])
+	fmt.Println("Slice :3 - ", sliceList[:3])
+
+// use of make
+	nums := make( []int, 3)
+
+
+	nums[0] = 1
+	nums[1] = 4
+	nums[2] = 3
+	fmt.Println("Nnmbers by using make :", nums)
+	
+	// nums[3] = 4 this give error
+
+	// use of append 
+		nums = append(nums, 6, 0) 
+		fmt.Println("Numbers by using make :", nums)
+
+		// sort method
+		fmt.Println("Numbers IntsAreSorted method:", 	sort.IntsAreSorted(nums))
+
+  sort.Ints(nums)
+		fmt.Println("Numbers sort method:", nums)
+	 
+		fmt.Println("Numbers IntsAreSorted method:", 	sort.IntsAreSorted(nums))
+
 }
