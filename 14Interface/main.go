81c8f4d78321c6944541695a4ea1ef1f050e6910
thejyotipatel <jyotipatelfyit77100@gmail.com>
2022-08-20 21:26:19 +0530

    interface

---
+++ 14Interface/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	radius float64
+
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius *  c.radius
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func getArea(s Shape) float64  {
+	return s.area()
+}
+*/
+
+
+
+func main() {
+	fmt.Println("interface")
+
+	/* circle := Circle{radius:3}
+	rectangle := Rectangle{width:2, height:3}
+
+	fmt.Println("Circle area:",  getArea(circle))
+	fmt.Println("Rectangle area:", getArea(rectangle)) */
+	
+}
