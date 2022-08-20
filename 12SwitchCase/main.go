2809c8c1e4343afac727707a52e1c0635d33ea63
thejyotipatel <jyotipatelfyit77100@gmail.com>
2022-08-21 00:06:35 +0530

    switch case

--- 12SwitchCase/main.go
+++ 12SwitchCase/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("interface")
+	fmt.Println("switch case")
+
+	// get random number
+	rand.Seed(time.Now().UnixNano())
+	diceNum := rand.Intn(6) + 1
+
+	fmt.Println("value :", diceNum)
+
+	switch diceNum{
+	case 1:
+		fmt.Println("value is 1 and you can open")
+	case 2:
+		fmt.Println("you can move 2 spot")
+	case 3:
+		fmt.Println("you can move 3 spot")
+		fallthrough
+	case 4:
+		fmt.Println("you can move 4 spot")
+	case 5:
+		fmt.Println("you can move 5 spot")
+	case 6:
+		fmt.Println("you can move 5 spot and roll agen")
+	default:
+		fmt.Println("try agen!")
+	}
 }
